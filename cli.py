fa1accabc7bf0142517d6bbcd1e935d6793bd722
AoiHosizora <a970335605@hotmail.com>
2020-10-17 21:08:40 +0800

    feat(generate): add basic function and center_image and en_punctuation

--- cli.py
+++ cli.py
@@ -1,26 +1,74 @@
+from os import path
+import re
 import click
+
 
 @click.group()
 def main():
+    """
+    Main command group.
+    """
     pass
 
-@main.command()
-@click.argument('arg')
-@click.option('--opt', '-o', type=str)
-def do(arg, opt):
-    """
-    do command
-    :param arg: The arg text
-    :param opt: The opt text
-    """
-    click.echo('arg: {}, opt: {}'.format(arg, opt))
 
 @main.command()
-def hello():
+@click.argument('input_file')
+@click.option('--output', '-o', type=str, required=True)
+@click.option('--force', type=bool, is_flag=True)
+@click.option('--center_image', type=bool, is_flag=True)
+@click.option('--en_punctuation', type=bool, is_flag=True)
+def generate(input_file: str, output: str, force: bool, center_image: bool, en_punctuation: bool):
     """
-    hello command
+    Generate markdown by options.
     """
-    click.echo('This is markdown-gen cli.')
+    force = not not force
+    center_image = not not center_image
+    en_punctuation = not not en_punctuation
+
+    if input_file == '':
+        raise Exception('The input file name could not be empty.')
+    elif not path.exists(input_file):
+        raise Exception('The input file "{}" does not exist.'.format(input_file))
+    elif not path.isfile(input_file):
+        raise Exception('The input file "{}" is a directory.'.format(input_file))
+
+    with open(input_file, 'r', encoding='utf-8') as fi:
+        # read
+        content = fi.read()
+
+        # 1. center image
+        if center_image:
+            center_image_re = re.compile(r'!\[(.*?)\]\((.+?)\)')
+            content = center_image_re.sub(r'<div align="center"><img src="\2" alt="\1" /></div>', content)
+
+        # 2. english punctuation
+        if en_punctuation:
+            punctuations = [
+                ['，', ', '], ['。', '. '], ['、', ', '], ['：', ': '], ['；', '; '], ['？', '? '], ['！', '! '], ['—', '-'],
+                ['“', ' "'], ['”', '" '], ['（', ' ('], ['）', ') '], ['【', ' ['], ['】', '] '], ['《', ' <'], ['》', '> '],
+            ]
+            for k, v in punctuations:
+                content = re.sub(f' *' + k + f' *', v, content)
+
+        # new line and space
+        content = re.sub(f' \n', '\n', content)
+        content = re.sub(f'\n ', '\n', content)
+
+        # save
+        if output == '':
+            raise Exception('The output file name could not be empty.')
+        elif path.exists(output):
+            if not path.isfile(output):
+                raise Exception('The output file "{}" is a directory.'.format(output))
+            elif not force:
+                raise Exception('The output file "{}" has been existed, please rename or use --force flag.'.format(output))
+        with open(output, 'w+', encoding='utf-8') as fo:
+            fo.write(content)
+
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as ex:
+        click.echo('Error: {}'.format(ex))
+        exit(1)
